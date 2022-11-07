1f8b8931415ff691109bd7993832271ed96679fb
jinwon93 <kjw8208@nate.com>
2022-11-07 19:43:39 +0900

    final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,22 +9,56 @@ import "./App.css";
 import Web3 from "web3";
 
 
-function App() {
-  return (
-    <EthProvider>
-      <div id="App" >
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shopInstance: null, // shopInstance 추가
+      myAccount: null,    // myAccount 추가
+      myApples: 0,        // myApples 추가
+      web3: null
+    };
+  }
+
+ 
+
+  buyApple() {
+    this.state.shopInstance.buyApple({
+      from: this.state.myAccount,
+      value: this.state.web3.toWei(10, "ether"),
+      gas: 900000
+    });
+  }
+
+  sellApple() {
+    this.state.shopInstance.sellMyApple(this.state.web3.toWei(10, "ether"), {
+      from: this.state.myAccount,
+      gas: 900000
+    });
+  }
+
+  updateMyApples() {
+    this.state.shopInstance.getMyApples().then(result => {
+      this.setState({ myApples: result.toNumber() });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>사과의 가격: 10 ETH</h1>
+        <button onClick={() => this.buyApple()}>구매하기</button>
+        <p>내가 가진 사과: {this.state.myApples}</p>
+        <button onClick={() => this.sellApple()}>
+          판매하기 (판매 가격: {10 * this.state.myApples})
+        </button>
       </div>
-    </EthProvider>
-  );
+    );
+  }
 }
+
 
 export default App;
